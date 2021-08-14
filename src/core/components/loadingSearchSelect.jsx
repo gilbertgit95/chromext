fae62903081586901f0f29b10bab9ae6f35f86f4
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2021-08-14 14:50:51 +0800

    midfy some components

--- src/core/components/loadingSearchSelect.jsx
+++ src/core/components/loadingSearchSelect.jsx
@@ -1,21 +1,53 @@
 import React, { useState } from 'react'
-import Button from '@material-ui/core/Button'
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import CloseIcon from '@material-ui/icons/Close'
+import Slide from '@material-ui/core/Slide'
+
+import MaterialTable from 'material-table'
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: 'fixed'
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const LoadingSearchSelect = (props) => {
 
   const [value, setValue] = useState(0)
+  const classes = useStyles()
+  const [open, setOpen] = useState(false)
+
+  const onOpen = (event) => {
+    console.log('open dialog')
+    setOpen(true);
+  }
+
+  const onClose = () => {
+    setOpen(false);
+  };
 
   const doThis = () => {
     setValue(value + 1)
   }
 
-  const onOpen = (event) => {
-    console.log('open dialog')
-  }
-
   return (
     <>
+      {/* button */}
         <Button
             onClick={onOpen}
             style={{
@@ -28,6 +60,25 @@ const LoadingSearchSelect = (props) => {
                 size="small"
                 color="primary" />
         </Button>
+        {/* dialog box */}
+        <Dialog fullScreen open={open} onClose={onClose} TransitionComponent={Transition}>
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton edge="start" color="inherit" onClick={onClose} aria-label="close">
+                <CloseIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                { props.dialogTitle? props.dialogTitle: 'Select Item' }
+              </Typography>
+              <Button autoFocus color="inherit" onClick={onClose}>
+                Done
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <div style={{ maxWidth: '100%', paddingTop: 50 }}>
+            <MaterialTable columns={props.columns} data={props.data} title='' />
+          </div>
+        </Dialog>
     </>
   )
 }
